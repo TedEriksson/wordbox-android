31fe1bb73f86d51cc8e497a30daa821d2e0a66ec
Botond Butuza <botondbutuza@gmail.com>
2015-03-28 13:04:34 +0000

    initial commit

---
+++ app/src/main/java/uk/co/botondbutuza/wordbox/Discover.java
@@ -0,0 +1,13 @@
+package uk.co.botondbutuza.wordbox;
+
+public class Discover
+{
+    String desc;
+    int hearts;
+
+    public Discover(String desc, int hearts)
+    {
+        this.desc = desc;
+        this.hearts = hearts;
+    }
+}
