466486a084f0a931102011bce8187aad8229184f
Ted <ted.eriksson@conjure.co.uk>
2015-04-05 21:18:23 +0100

    add real server

--- app/src/main/java/uk/co/vism/wordbox/network/RestClient.java
+++ app/src/main/java/uk/co/vism/wordbox/network/RestClient.java
@@ -6,7 +6,7 @@ import org.androidannotations.annotations.rest.Get;
 import org.androidannotations.annotations.rest.Rest;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 
-@Rest(rootUrl = "http://private-328f8-wordbox.apiary-mock.com", converters = {GsonHttpMessageConverter.class})
+@Rest(rootUrl = "https://wordbox.herokuapp.com", converters = {GsonHttpMessageConverter.class})
 public interface RestClient {
     @Get("/users/{userId}")
     JsonObject getUser(int userId);
